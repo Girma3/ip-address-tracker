7eda9ce737e885b7b52186872a6e2bc927a84f0e
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-03-09 06:40:13 +0300

    base layers added to the map

--- src/index.js
+++ src/index.js
@@ -4,36 +4,69 @@ import L from 'leaflet'
 import iconLocation from './assets/images/icon-location.svg'
 import { getGeolocationdata } from './functions.js'
 let userLongtiude;
-let UserLatitude;
+let userLatitude;
+let userCountryName;
+let userPlaceName;
 // api to get long and lat
 
 getGeolocationdata().then(response => {
-  UserLatitude = Number(response.latitude)
+  userLatitude = Number(response.latitude)
   userLongtiude = Number(response.longitude)
+  userCountryName = response.location.country
+  userPlaceName = response.location.region
 
-  maps(UserLatitude, userLongtiude)
+  maps(userLatitude, userLongtiude)
 }).catch(err => {
   console.log(err, "can't find data")
 })
+// layers
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap'
+});
+
+const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  attribution: '© Google map'
+});
+const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  attribution: '© Google map'
+});
+const googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+  maxZoom: 20,
+  subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  attribution: '© Google map'
+});
+const baseMaps = {
+  OpenStreetMap: osm,
+  satellite: googleSat,
+  street: googleStreets,
+  hybrid: googleHybrid
+};
 
 // add map to the screen
 
 // const map = L.map('map').flyTo([UserLatitude, userLongtiude], 20);
 function maps (lat, lon) {
-  const map = L.map('map').setView([lat, lon], 13);
-  const marker = L.marker([lat, lon]).addTo(map)
-  marker.bindPopup('<b>based on your ip your address is here').openPopup();
-  // icon
-  const greenIcon = L.icon({
+  // default map layer is osm
+  const map = L.map('map', { layers: [osm] }).setView([lat, lon], 13);
+  // add layer control
+  L.control.layers(baseMaps).addTo(map);
+  // pop up message
+  const message = `<b>your ip address located in ${userPlaceName}, ${userCountryName}`
+  const marker = L.marker([lat, lon], { alt: `${userPlaceName}` }).addTo(map).bindPopup(`${message}`).openPopup();
+  // add icon
+  const blackIcon = L.icon({
     iconUrl: `${iconLocation}`,
-    iconSize: [40, 65] // size of the icon
+    iconSize: [40, 65], // size of the icon
+    iconAnchor: [16, 80]
+
   });
-  L.marker([lat, lon], { icon: greenIcon }).addTo(map);
-
-  console.log(userLongtiude, UserLatitude)
-
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+  // add event on icon click
+  L.marker([lat, lon], { icon: blackIcon }).addTo(map).on('click', () => {
+    marker.bindPopup(`${message}`).openPopup();
+  });
 }
