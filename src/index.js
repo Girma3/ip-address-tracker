3d3d4306be51f6f5fdbd28b7dc5fd48442237f0c
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-03-09 15:44:15 +0300

    function to get by ip and domain changed

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import 'leaflet/dist/leaflet.css'
 import './style.css'
 import L from 'leaflet'
 import iconLocation from './assets/images/icon-location.svg'
-import { getGeolocationdata } from './functions.js'
-let userLongtiude;
-let userLatitude;
+import { getGeolocationdata, getbyDefaultipadress, getbyDomain, getbyIpadress, focusStyle } from './functions.js'
+
+let latitude;
+let longitude;
 let userCountryName;
 let userPlaceName;
-const skeletonPage = document.querySelector('main');
+// const skeletonPage = document.querySelector('main');
+const errorMsg = document.querySelector('[data-error-msg]')
+const userInput = document.querySelector('.user-input');
+const submitBtn = document.querySelector('[data-location-btn]');
 const userIp = document.querySelector('[data-ip-address]');
 const userLocation = document.querySelector('[data-user-location]');
 const userTimezone = document.querySelector('[data-user-timezone]')
 const userIsp = document.querySelector('[data-user-isp]');
 const userAsn = document.querySelector('[data-user-asn]');
-
+// function that clear existing map to redraw again
+function clearMap () {
+  const container = L.DomUtil.get('map');
+  if (container != null) {
+    container._leaflet_id = null;
+  }
+}
 // base layers for the map
 const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
@@ -46,7 +56,8 @@ const baseMaps = {
 *parmaeters: latitude,longitude, icon,placename,and country name
 *return map with given  positon at center and popup message
 */
-function maps (lat, lon) {
+
+async function maps (lat, lon) {
   // default map layer is osm
   const map = L.map('map', { layers: [osm] }).setView([lat, lon], 13);
   // add layer control
@@ -59,28 +70,82 @@ function maps (lat, lon) {
     iconUrl: `${iconLocation}`,
     iconSize: [40, 65], // size of the icon
     iconAnchor: [16, 80]
-
   });
   // add event on icon click
   L.marker([lat, lon], { icon: blackIcon }).addTo(map).on('click', () => {
     marker.bindPopup(`${message}`).openPopup();
   });
 }
-getGeolocationdata().then(response => {
-  userLatitude = Number(response.latitude)
-  userLongtiude = Number(response.longitude)
-  userCountryName = response.countryName
-  userPlaceName = response.region
-  // update dom
+
+function updateDom (response) {
   userIp.textContent = response.ipAdress;
   userIsp.textContent = response.isp;
   userTimezone.textContent = response.timeZone;
   userLocation.textContent = `${response.region}, ${response.countryName}`;
   userAsn.textContent = response.asnNumber;
-  //   console.log(userCountryName)
-  console.log(response)
-
-  maps(userLatitude, userLongtiude)
-}).catch(err => {
-  console.log(err, "can't find data")
+}
+const locationBydefault = getbyDefaultipadress().then(response => {
+  return response
+});
+const data = getGeolocationdata(locationBydefault)
+data.then(response => {
+  latitude = Number(response.latitude);
+  longitude = Number(response.longitude);
+  userCountryName = response.countryName;
+  userPlaceName = response.region;
+  updateDom(response)
+  maps(latitude, longitude)
+}).catch(error => {
+  console.log(error)
+  errorMsg.textContent = 'no data found Please, try later'
+})
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const value = userInput.value;
+  const regexIp = /^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$/;
+  const regexDomain = /^(?!-)(?!:\/\/)([a-zA-Z0-9-]+\.){0,5}[a-zA-Z0-9-][a-zA-Z0-9-]+\.[a-zA-Z]{2,64}?$/;
+  console.log(regexDomain.test(value))
+  if (regexIp.test(value)) {
+    const locationByip = getbyIpadress(value).then(response => {
+      return response
+    });
+    const data = getGeolocationdata(locationByip)
+    data.then(response => {
+      latitude = Number(response.latitude);
+      longitude = Number(response.longitude);
+      userCountryName = response.countryName;
+      userPlaceName = response.region;
+      clearMap();
+      maps(latitude, longitude);
+      updateDom(response);
+    }).catch(err => {
+      console.log(err)
+      userInput.classList.add('invalid');
+      errorMsg.textContent = 'no data found in this ip address';
+    });
+  } else if (regexDomain.test(value)) {
+    const locationBydomain = getbyDomain(value).then(response => {
+      return response
+    })
+    const data = getGeolocationdata(locationBydomain)
+    data.then(response => {
+      latitude = Number(response.latitude);
+      longitude = Number(response.longitude);
+      userCountryName = response.countryName;
+      userPlaceName = response.region;
+      clearMap();
+      maps(latitude, longitude);
+      updateDom(response);
+    }).catch(err => {
+      console.log(err)
+      userInput.classList.add('invalid');
+      errorMsg.textContent = 'no data found by this domain'
+    });
+  } else if (regexIp.test(value) === false && regexDomain.test(value) === false) {
+    userInput.classList.add('invalid');
+    errorMsg.textContent = 'no data found please,check the format.(ip start from = 0.0.0.0  & domain = example.com)'
+  }
+})
+userInput.addEventListener('focus', () => {
+  focusStyle(userInput, errorMsg)
 })
